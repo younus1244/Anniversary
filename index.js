f8d7c5cd6ab364cd5ea284a3492015aa0544f56b
younus1244 <141546812+younus1244@users.noreply.github.com>
2024-01-18 20:16:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/gbICivOO26U?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("img src="IMG-20220831-WA0030.jpg");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
